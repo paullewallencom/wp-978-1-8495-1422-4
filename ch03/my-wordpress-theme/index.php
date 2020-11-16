fa8a8e8fb464dd13268f699d6acc4ab38b42e253
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-11-16 06:27:18 -0800

    Adding another autogenerated class
    Adding another autogenerated class.

--- ch03/my-wordpress-theme/index.php
+++ ch03/my-wordpress-theme/index.php
@@ -53,7 +53,7 @@
                 <?php while (have_posts()) : the_post();?>
                 
                 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                    <h2 class="">
+                    <h2 class="post-title">
                         <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
                             <?php the_title();?>
                         </a>
