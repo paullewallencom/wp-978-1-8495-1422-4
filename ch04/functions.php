e0b923f6714b7c544fd407d99407ea5dfb2a45f4
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-11-16 19:42:15 -0800

    Registering sidebars in functions.php
    Registering sidebars in functions.php.

--- ch04/functions.php
+++ ch04/functions.php
@@ -6,4 +6,32 @@
         );
     }
     add_action( 'init', 'register_my_menus' );
+
+    function osmag_widgets_init() {
+        register_sidebar (array(
+          'name'          => __('Sidebar','osmag'),
+          'id'            => "sidebar-widget-area",
+          'before_widget' => '<li id="%1$s" class="widget %2$s">',
+          'after_widget'  => '</li>',
+          'before_title'  => '<h2 class="widgettitle">',
+          'after_title'   => '</h2>' )
+          );
+        register_sidebar (array(
+          'name'          => __('Left Footer','osmag'),
+          'id'            => "footer-left-widget-area",
+          'before_widget' => '<li id="%1$s" class="widget %2$s">',
+          'after_widget'  => '</li>',
+          'before_title'  => '<h2 class="widgettitle">',
+          'after_title'   => '</h2>' )
+          );
+        register_sidebar (array(
+          'name'          => __('Right Footer','osmag'),
+          'id'            => "footer-right-widget-area",
+          'before_widget' => '<li id="%1$s" class="widget %2$s">',
+          'after_widget'  => '</li>',
+          'before_title'  => '<h2 class="widgettitle">',
+          'after_title'   => '</h2>' )
+          );
+      }
+      add_action('init', 'osmag_widgets_init');
 ?>
