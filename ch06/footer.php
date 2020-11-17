5614e513356078d709310ffa341a2ea002e04be9
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-11-16 20:13:13 -0800

    Reorganizing
    Reorganizing

---
+++ ch06/footer.php
@@ -0,0 +1,41 @@
+<footer>
+    <aside class="footer first">
+    <?php if ( is_active_sidebar( 'footer-first-widget-area' ) ) : ?>
+            <?php dynamic_sidebar( 'footer-first-widget-area' ); ?>
+    <?php endif; ?>
+    </aside>
+    
+    <aside class="footer second">
+    <?php if ( is_active_sidebar( 'footer-second-widget-area' ) ) : ?>
+            <?php dynamic_sidebar( 'footer-second-widget-area' ); ?>
+    <?php endif; ?>
+    </aside>
+    
+    <aside class="footer third">
+    <?php if ( is_active_sidebar( 'footer-third-widget-area' ) ) : ?>
+            <?php dynamic_sidebar( 'footer-third-widget-area' ); ?>
+    <?php endif; ?>
+    </aside>
+    
+    <aside class="footer fourth">
+    <?php if ( is_active_sidebar( 'footer-fourth-widget-area' ) ) : ?>
+            <?php dynamic_sidebar( 'footer-fourth-widget-area' ); ?>
+    <?php endif; ?>
+    </aside>
+    
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
+
+<!--         <h3>Socialize</h3> -->
+<!--         <ul class="grid4up"> -->
+<!--             <li><a href="#" class="soc facebook">facebook</a></li> -->
+<!--             <li><a href="#" class="soc twitter">twitter</a></li> -->
+<!--             <li><a href="#" class="soc rss">rss</a></li> -->
+<!--             <li><a href="#" class="soc google">gmail</a></li> -->
+<!--         </ul> -->
+<!-- 
+<!--         <div class="push"></div> -->
+<!--         <h3>About us</h3> -->
+<!--         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p> -->
---
+++ ch06/index.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+
+<div id="container2" class="bdr bdr-top">
+
+<div class="content left two-thirds">
+<h2 class="thisMonth embossed" style="color:#fff;">This Month:</h2>
+
+<?php if (have_posts()) :?>
+	<?php while (have_posts()) : the_post();?> 
+
+	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+		<h2 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title();?></a></h2>
+		<p class="entry-meta">by <?php the_author_meta('first_name'); ?> <?php the_author_meta('last_name'); ?> in <?php the_category(", ") ?></p>
+
+		<div class="entry-content"><!--//post-->
+			<?php if ( has_post_thumbnail() ) {
+				the_post_thumbnail('medium');
+			} ?>
+			<?php the_content();?>
+		</div><!--//.entry-content-->
+
+		<p class="left"><a class="more" href="<?php the_permalink(); ?>">Read more &raquo;</a></p>
+		<p class="right"><a class="comments-count" href="<?php the_permalink(); ?>"><?php comments_number('0', '1', '%'); ?></a></p>
+		<div class="push"></div>
+		
+	</article>
+<?php endwhile; ?>
+<?php else : ?>
+       <h2 class="center">Not Found</h2>
+       <p class="center">Sorry, but you are looking for something that isn't here.</p>
+       <?php get_search_form(); ?>
+<?php endif; ?>
+
+
+<div class="push"></div>
+</div><!--content-->
+
+<?php get_sidebar(); ?>
+
+<div class="push"></div>
+
+</div><!--//#container2-->
+
+</div><!--//#container-->
+<div id="across">
+
+<?php get_footer(); ?>
