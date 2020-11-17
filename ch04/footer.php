df227d837de6cc1b97efc084d3cdb94f41900eb8
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-11-16 19:51:57 -0800

    Adding footer widgets
    Adding footer widgets.

--- ch04/footer.php
+++ ch04/footer.php
@@ -18,3 +18,15 @@
 <?php wp_footer(); ?>
 </body>
 </html>
+
+<!--         <h3>Socialize</h3> -->
+<!--         <ul class="grid4up"> -->
+<!--             <li><a href="#" class="soc facebook">facebook</a></li> -->
+<!--             <li><a href="#" class="soc twitter">twitter</a></li> -->
+<!--             <li><a href="#" class="soc rss">rss</a></li> -->
+<!--             <li><a href="#" class="soc google">gmail</a></li> -->
+<!--         </ul> -->
+<!-- 
+<!--         <div class="push"></div> -->
+<!--         <h3>About us</h3> -->
+<!--         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p> -->
